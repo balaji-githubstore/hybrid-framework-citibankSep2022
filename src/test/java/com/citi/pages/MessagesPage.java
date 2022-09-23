ee93f5a0139e16872212478391f330acd4addcd6
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2022-09-23 13:10:47 +0530

    added comment in messages

--- src/test/java/com/citi/pages/MessagesPage.java
+++ src/test/java/com/citi/pages/MessagesPage.java
@@ -1,7 +1,12 @@
  package com.citi.pages;
 
 import org.openqa.selenium.WebDriver;
-
+/**
+ * This page handles elements in Messages Menu
+ * 
+ * @author Balaji
+ *
+ */
 public class MessagesPage {
 	
 	private WebDriver driver;
